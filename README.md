f0b05df5cd2af13581b404a919b920eda352fe29
Gavin Didrichsen <gavin.didrichsen@gmail.com>
2020-06-03 05:49:59 +0100

    Add inspec tests
    Signed-off-by: Gavin Didrichsen <gavin.didrichsen@gmail.com>

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![Build Status](https://dev.azure.com/chefcorp-partnerengineering/Chef%20Base%20Plans/_apis/build/status/chef-base-plans.minio?branchName=master)](https://dev.azure.com/chefcorp-partnerengineering/Chef%20Base%20Plans/_build/latest?definitionId=69&branchName=master)
+
 # Minio
 
 Minio is a high performance distributed object storage server, designed for large-scale private cloud infrastructure.
---
+++ controls/minio_exists.rb
@@ -0,0 +1,23 @@
+title 'Tests to confirm minio exists'
+
+plan_origin = ENV['HAB_ORIGIN']
+plan_name = input('plan_name', value: 'minio')
+
+control 'core-plans-minio-exists' do
+  impact 1.0
+  title 'Ensure minio exists'
+  desc '
+  Verify minio by ensuring /bin/minio exists'
+  
+  plan_installation_directory = command("hab pkg path #{plan_origin}/#{plan_name}")
+  describe plan_installation_directory do
+    its('exit_status') { should eq 0 }
+    its('stdout') { should_not be_empty }
+  end
+
+  command_relative_path = input('command_relative_path', value: '/bin/minio')
+  command_full_path = plan_installation_directory.stdout.strip + "#{command_relative_path}"
+  describe file(command_full_path) do
+    it { should exist }
+  end
+end
