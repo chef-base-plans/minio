2544ddbf833e587c1c691d8b8c2b1185b6af6200
Gavin Didrichsen <gavin.didrichsen@gmail.com>
2020-06-10 12:54:30 +0100

    Simplify the test code by: * removing 'DEBUG=true' * preferring full installation path to binary instead of 'hab pkg exec' * parsing the version from the full installation path
    Signed-off-by: Gavin Didrichsen <gavin.didrichsen@gmail.com>

--- controls/minio_works.rb
+++ controls/minio_works.rb
@@ -2,6 +2,7 @@ title 'Tests to confirm minio works as expected'
 
 plan_origin = ENV['HAB_ORIGIN']
 plan_name = input('plan_name', value: 'minio')
+command_relative_path = input('command_relative_path', value: 'bin/minio')
 
 control 'core-plans-minio-works' do
   impact 1.0
@@ -9,10 +10,10 @@ control 'core-plans-minio-works' do
   desc '
   Verify minio by ensuring 
   (1) its installation directory exists and 
-  (2) that it returns the expected version.  Since minio --help returns a version
-  like "2019-07-31T18:57:56Z" with colons but the plan_pkg_version (derived from the plan_pkg_ident)
-  only contains hyphens "-", then logical equivalence is achieved by transforming 
-  plan_pkg_version ==> minio_version_with_colons below
+  (2) that it returns the expected version.  Since some platforms return slightly
+  different formats of minio version, some with colons like "2019-07-31T18:57:56Z" 
+  others without like "2019-07-31T18:57:56Z", then only the date portion is being 
+  used for a successful match.
   '
   
   plan_installation_directory = command("hab pkg path #{plan_origin}/#{plan_name}")
@@ -21,14 +22,12 @@ control 'core-plans-minio-works' do
     its('stdout') { should_not be_empty }
   end
   
-  plan_pkg_ident = ((plan_installation_directory.stdout.strip).match /(?<=pkgs\/)(.*)/)[1]
-  plan_pkg_version = (plan_pkg_ident.match /^#{plan_origin}\/#{plan_name}\/(?<version>.*)\//)[:version]
-  version_element = plan_pkg_version.split("-")
-  minio_version_with_colons = "#{version_element[0]}-#{version_element[1]}-#{version_element[2]}:#{version_element[3]}:#{version_element[4]}"
-  describe command("DEBUG=true; hab pkg exec #{plan_pkg_ident} minio --help") do
+  plan_pkg_version = plan_installation_directory.stdout.split("/")[5]
+  command_full_path = File.join(plan_installation_directory.stdout.strip, "#{command_relative_path}")
+  describe command("#{command_full_path} --help") do
     its('exit_status') { should eq 0 }
     its('stdout') { should_not be_empty }
-    its('stdout') { should match /#{minio_version_with_colons}/ }
+    its('stdout') { should match /#{plan_pkg_version.split("T")[0]}/ }
     its('stderr') { should be_empty }
   end
 end
