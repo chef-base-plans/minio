feb10fbfb9b620cfdb3a2a7cd7ebd70f06aa06ff
Gavin Didrichsen <gavin.didrichsen@gmail.com>
2020-07-01 14:22:55 +0100

    Update documentation adding habitat usage instruction and other fixes
    Signed-off-by: Gavin Didrichsen <gavin.didrichsen@gmail.com>

--- controls/minio_works.rb
+++ controls/minio_works.rb
@@ -2,15 +2,15 @@ title 'Tests to confirm minio works as expected'
 
 plan_origin = ENV['HAB_ORIGIN']
 plan_name = input('plan_name', value: 'minio')
-command_relative_path = input('command_relative_path', value: 'bin/minio')
+
 
 control 'core-plans-minio-works' do
   impact 1.0
   title 'Ensure minio works as expected'
   desc '
-  Verify minio by ensuring 
-  (1) its installation directory exists and 
-  (2) that it returns the expected version.  Since some platforms return slightly
+  Verify minio by ensuring that
+  (1) its installation directory exists 
+  (2) it returns the expected version.  Since some platforms return slightly
   different formats of minio version, some with colons like "2019-07-31T18:57:56Z" 
   others without like "2019-07-31T18:57:56Z", then only the date portion is being 
   used for a successful match.
@@ -20,10 +20,11 @@ control 'core-plans-minio-works' do
   describe plan_installation_directory do
     its('exit_status') { should eq 0 }
     its('stdout') { should_not be_empty }
+    its('stderr') { should be_empty }
   end
   
   plan_pkg_version = plan_installation_directory.stdout.split("/")[5]
-  command_full_path = File.join(plan_installation_directory.stdout.strip, "#{command_relative_path}")
+  command_full_path = File.join(plan_installation_directory.stdout.strip, "bin", "minio")
   describe command("#{command_full_path} --help") do
     its('exit_status') { should eq 0 }
     its('stdout') { should_not be_empty }
