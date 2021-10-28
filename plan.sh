a0af789b0bba75b90238d9b125a137a42765ac85
Nimit <nimit.jyotiana@hotmail.com>
2021-10-28 12:29:08 +0530

    upgraded version to v2021-10-27T16-29-42Z
    Signed-off-by: Nimit <nimit.jyotiana@hotmail.com>

--- plan.sh
+++ plan.sh
@@ -1,10 +1,10 @@
 pkg_name=minio
 pkg_origin=core
-pkg_version="2021-03-17T02-33-02Z"
+pkg_version=2021-10-27T16-29-42Z
 pkg_description="Minio is a high performance distributed object storage server, designed for large-scale private cloud infrastructure."
 pkg_upstream_url="https://minio.io"
 pkg_source="https://dl.minio.io/server/minio/release/linux-amd64/archive/minio.RELEASE.${pkg_version}"
-pkg_shasum=475f878614021769dba33fc051124834ac2131b52e52794f83f7c21aea51444a
+pkg_shasum=05cfb434f50ee4f9ab897c8c9bdc921d7609ac66a64c1678d0fbaa61fb3d12fd
 pkg_license=('Apache-2.0')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_bin_dirs=(bin)
